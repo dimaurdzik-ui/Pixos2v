59361b66eba1037c8d894174bda7b9aa92e67b7d
Dmytro Urdzik <ai_dmytro@ukrcoder.com.ua>
2026-05-26 15:05:52 +0200

    chore: remove debug scripts

--- test_routes.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from apps.api.app.main import app
-
-def check():
-    for route in app.routes:
-        print(getattr(route, "methods", None), getattr(route, "path", route.name))
-
-check()
